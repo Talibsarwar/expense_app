bb0dd0b78105f786621eff07e5cfa87d423371ae
Talibsarwar <47272482+Talibsarwar@users.noreply.github.com>
2020-06-22 18:52:14 +0000

    Update

---
+++ src/child.js
@@ -0,0 +1,59 @@
+import React, { useContext, useState } from 'react';
+import {TransactionContext} from './transContext';
+
+function Child() {
+    let {transactions, addTransaction} = useContext(TransactionContext);
+
+    let [newDesc, setDesc] = useState("");
+    let [newAmount, setAmount] = useState(0);
+
+    const handleAddition = (event)=> {
+            event.preventDefault();
+            addTransaction({
+                amount: newAmount,
+                desc: newDesc
+            })
+    }
+    return (
+        <div className="container">
+            <h1 className="text-center">Expense Tracker</h1>
+
+            <h3>Your Balance <br /> PKR3000</h3>
+
+            <div className="expense-container">
+                <h3>INCOME <br /> PKR3000</h3>
+                <h3>EXPENSE <br /> PKR900</h3>
+            </div>
+            
+            <div className="history">History</div>
+            <hr />
+            <ul className="transaction-list">
+                {transactions.map((transObj, ind)=>{
+                    return(<li>
+                        <span>{transObj.desc}</span>
+                        <span>{transObj.amount}</span>
+                    </li>)
+                })}
+                
+            </ul>
+            <h3>Add new transection</h3>
+            <hr />
+
+            <form className="transaction-form" onSubmit={handleAddition}>
+                <label>
+                      Enter Description <br />
+                      <input type="text" onChange={(ev)=>setDesc(ev.target.value)} required/>
+                </label>
+                <br />
+                <label>
+                      Enter Amount <br />
+                      <input type="number" onChange={(ev)=>setAmount(ev.target.value)} required/>
+                </label>
+                <br />
+                <input type="submit" value="Add Transaction" />
+            </form>
+        </div>
+    );
+}
+
+export default Child;
---
+++ src/transContext.js
@@ -0,0 +1,35 @@
+import React, { createContext, useReducer } from 'react';
+import TransactionReducer from './transReducer';
+
+const initialTransaction = [
+{amount: 800, desc: "Cash"},
+{amount: -40, desc: "Book"},
+{amount: -200, desc: "Camera"}  
+]
+
+export const TransactionContext = createContext(initialTransaction);
+
+
+
+export const TransactionProvider = ({children})=> {
+    let [state, dispatch] = useReducer(TransactionReducer, initialTransaction);
+
+    function addTransaction(transObj){
+        dispatch({
+            type: "ADD TRANSACTION",
+            payload: {
+                amount: transObj.amount,
+                desc: transObj.desc
+            },
+        })
+    }
+
+    return (
+        <TransactionContext.Provider value={{
+            transactions: state,
+            addTransaction
+        }}>
+           {children}
+        </TransactionContext.Provider>
+    )
+};
