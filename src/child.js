b5c9ea84fc4894ad51609991ef58dafe73d611f2
Talibsarwar <47272482+Talibsarwar@users.noreply.github.com>
2020-06-22 20:30:06 +0000

    UpdateFinal1

--- src/child.js
+++ src/child.js
@@ -1,76 +1,86 @@
 import React, { useContext, useState } from 'react';
-import {TransactionContext} from './transContext';
+import { TransactionContext } from './transContext';
 
 function Child() {
-    let {transactions, addTransaction} = useContext(TransactionContext);
+    let { transactions, addTransaction } = useContext(TransactionContext);
 
     let [newDesc, setDesc] = useState("");
     let [newAmount, setAmount] = useState(0);
 
-    const handleAddition = (event)=> {
-            event.preventDefault();
-            if(Number(newAmount) === 0){
-                alert("Please enter correct value");
-                return false;
-            }
-            addTransaction({
-                amount: Number(newAmount),
-                desc: newDesc
-            })
+    const handleAddition = (event) => {
+        event.preventDefault();
+        if (Number(newAmount) === 0) {
+            alert("Please enter correct value");
+            return false;
+        }
+        addTransaction({
+            amount: Number(newAmount),
+            desc: newDesc
+        });
+        setDesc('');
+        setAmount(0)
     }
 
     const getIncome = () => {
         let income = 0;
-        for (var i = 0; i < transactions.length; i++){
-         if (transactions[i].amount > 0)
-            income += transactions[i].amount
+        for (var i = 0; i < transactions.length; i++) {
+            if (transactions[i].amount > 0)
+                income += transactions[i].amount
         }
         return income;
     }
 
-const getExpense = () => {
-    let expense = 0;
-    for (var i = 0; i < transactions.length; i++){
-        if (transactions[i].amount < 0)
-            expense += transactions[i].amount
+    const getExpense = () => {
+        let expense = 0;
+        for (var i = 0; i < transactions.length; i++) {
+            if (transactions[i].amount < 0)
+                expense += transactions[i].amount
+        }
+        return expense;
     }
-    return expense;
-}
 
     return (
         <div className="container">
             <h1 className="text-center">Expense Tracker</h1>
 
-            <h3>Your Balance <br /> {getIncome() + getExpense()}</h3>
+            <h3>Your Balance <br /> PKR{getIncome() + getExpense()}</h3>
 
             <div className="expense-container">
-                <h3>INCOME <br /> {getIncome()}</h3>
-                <h3>EXPENSE <br /> {getExpense()}</h3>
+                <h3>INCOME <br /> PKR{getIncome()}</h3>
+                <h3>EXPENSE <br /> PKR{getExpense()}</h3>
             </div>
-            
+
             <div className="history">History</div>
             <hr />
             <ul className="transaction-list">
-                {transactions.map((transObj, ind)=>{
-                    return(<li>
+                {transactions.map((transObj, ind) => {
+                    return (<li>
                         <span>{transObj.desc}</span>
-                        <span>{transObj.amount}</span>
+                        <span>PKR{transObj.amount}</span>
                     </li>)
                 })}
-                
+
             </ul>
-            <h3>Add new transection</h3>
+            <h3>Add new transaction</h3>
             <hr />
 
             <form className="transaction-form" onSubmit={handleAddition}>
                 <label>
-                      Enter Description <br />
-                      <input type="text" onChange={(ev)=>setDesc(ev.target.value)} required/>
+                    Enter Description <br />
+                    <input type="text"
+                        value={newDesc}
+                        placeholder="Description"
+                        onChange={(ev) => setDesc(ev.target.value)}
+                        required />
                 </label>
                 <br />
                 <label>
-                      Enter Amount <br />
-                      <input type="number" onChange={(ev)=>setAmount(ev.target.value)} required/>
+                    Enter Amount <br />
+                    <input type="number"
+                        value={newAmount}
+                        placeholder="Amount"
+                        onChange={(ev) => setAmount(ev.target.value)}
+                        required />
                 </label>
                 <br />
                 <input type="submit" value="Add Transaction" />
