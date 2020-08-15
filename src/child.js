825110df6d4d36768bc2b63c4a65043109617bcb
Talibsarwar <47272482+Talibsarwar@users.noreply.github.com>
2020-08-15 12:10:16 +0000

    Update

--- src/child.js
+++ src/child.js
@@ -90,7 +90,7 @@ function Child() {
                 </label>
                 <br />
                 <input type="submit" value="Add Transaction" />
-                <input type="submit" value="Allow the Notification" onClick={initNotification} />
+                <input type="submit" value="Allow Notifications" onClick={initNotification} />
             </form>
         </div>
     );
